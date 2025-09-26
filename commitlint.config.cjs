b559d720ef763aae28cfe6934e1363be7c8b8bc7
Abu Talha <rogdex24@gmail.com>
2025-09-26 22:34:02 +0530

    chore: allow detailed commit titles up to 120 characters

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -15,11 +15,11 @@ module.exports = {
         'refactor',
         'revert',
         'style',
-        'test'
-      ]
+        'test',
+      ],
     ],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 72]
-  }
+    'header-max-length': [2, 'always', 120],
+  },
 };
